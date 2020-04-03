4bc0ac2d3b790e223538f164703e3d5201fd04c0
perawatt <pekung4433@perfenterprise.com>
2020-04-03 11:57:00 +0700

    style home-feed (incomplete)

---
+++ src/app/home-feed/home-feed.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home-feed',
+  templateUrl: './home-feed.page.html',
+  styleUrls: ['./home-feed.page.scss'],
+})
+export class HomeFeedPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
