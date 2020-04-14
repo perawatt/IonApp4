b05a6ccd22ef2adca064c437c0b86b37caae7781
perawatt <pekung4433@perfenterprise.com>
2020-04-14 17:14:30 +0700

    binding user-profile-new-address

--- src/app/user-profile-new-address/user-profile-new-address.page.ts
+++ src/app/user-profile-new-address/user-profile-new-address.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { IonManaLib } from 'ion-m-lib';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-user-profile-new-address',
@@ -7,9 +9,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserProfileNewAddressPage implements OnInit {
 
-  constructor() { }
+  public hasLoaded: string;
+  public fg: FormGroup;
+  private mcontentid = "user-profile-new-address";
+
+  constructor(private fb: FormBuilder, private svc: IonManaLib) { 
+    this.fg = this.fb.group({
+      'title': [null, Validators.required],
+      'streetAddress': [null, Validators.required],
+      'district': [null, Validators.required],
+      'city': [null, Validators.required],
+      'province': [null, Validators.required],
+      'postalCode': [null, Validators.required],
+      'telephoneNumber': null,
+      'mobileNumber': null,
+      'fullAddress': null
+    });
+
+    this.fg.valueChanges.subscribe(_ => {
+      this.svc.validForm(this.fg.valid)
+    });
+  }
 
   ngOnInit() {
+  }
+
+  ionViewDidEnter() {
+    this.svc.initPageApi(this.mcontentid);
+    this.svc.validForm(this.fg.valid);
+  }
+
+  onSave() {
+    if (this.fg.valid) {
+      this.svc.submitFormData(this.mcontentid, this.fg.value);
+    }
   }
 
 }
