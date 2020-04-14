63e9505af1bc1b76bdcc41a49c301e53ba2e9747
perawatt <pekung4433@perfenterprise.com>
2020-04-14 17:38:52 +0700

    binding user-profile-edit-address

--- src/app/user-profile-edit-address/user-profile-edit-address.module.ts
+++ src/app/user-profile-edit-address/user-profile-edit-address.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
 import { UserProfileEditAddressPageRoutingModule } from './user-profile-edit-address-routing.module';
 
 import { UserProfileEditAddressPage } from './user-profile-edit-address.page';
+import { DirectivesModule } from '../../directives/directives.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
+    ReactiveFormsModule,
+    DirectivesModule,
     UserProfileEditAddressPageRoutingModule
   ],
   declarations: [UserProfileEditAddressPage]
--- src/app/user-profile-edit-address/user-profile-edit-address.page.ts
+++ src/app/user-profile-edit-address/user-profile-edit-address.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { IonManaLib } from 'ion-m-lib';
 
 @Component({
   selector: 'app-user-profile-edit-address',
@@ -7,9 +9,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserProfileEditAddressPage implements OnInit {
 
-  constructor() { }
+  public hasLoaded: string;
+  public fg: FormGroup;
+  public formData$: Promise<{}> = new Promise<{}>(_ => { });
+  private mcontentid = "user-profile-edit-address";
+
+  constructor(private fb: FormBuilder, private svc: IonManaLib) {
+    this.fg = this.fb.group({
+      'address': this.fb.group({
+        'title': [null, Validators.required],
+        'streetAddress': [null, Validators.required],
+        'district': [null, Validators.required],
+        'city': [null, Validators.required],
+        'province': [null, Validators.required],
+        'postalCode': [null, Validators.required],
+        'telephoneNumber': null,
+        'mobileNumber': null,
+        'fullAddress': null,
+      }),
+      'isBillingAddress': null,
+      'isShippingAddress': null
+    });
+
+    this.fg.valueChanges.subscribe(_ => {
+      this.svc.validForm(this.fg.valid)
+    });
+  }
 
   ngOnInit() {
+  }
+
+  ionViewDidEnter() {
+    this.hasLoaded = null;
+    let load$ = this.loadData$();
+    this.formData$ = load$;
+    load$.then(it => {
+      this.svc.validForm(this.fg.valid);
+      this.hasLoaded = it ? "y" : "n";
+    });
+  }
+
+  private loadData$() {
+    return this.svc.initPageApi(this.mcontentid)
+      .then(_ => {
+        return this.svc.getApiData(this.mcontentid);
+      })
+  }
+
+  onSave() {
+    if (this.fg.valid) {
+      this.svc.submitFormData(this.mcontentid, this.fg.value);
+    }
   }
 
 }
