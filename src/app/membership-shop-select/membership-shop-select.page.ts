75d15f4cd4e246a99705dbf5002e72ab1327bea9
Anawat <anawat@perfenterprise.com>
2020-04-14 15:08:18 +0700

    binding membership-shop-select

--- src/app/membership-shop-select/membership-shop-select.page.ts
+++ src/app/membership-shop-select/membership-shop-select.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { IonManaLib } from 'ion-m-lib';
 
 @Component({
   selector: 'app-membership-shop-select',
@@ -7,9 +8,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MembershipShopSelectPage implements OnInit {
 
-  constructor() { }
+  public hasLoaded: string;
+  public data$ = Promise.resolve<{}>({});
+  private mcontentid = "membership-shop-select";
+
+  constructor(private svc: IonManaLib) { }
 
   ngOnInit() {
   }
 
+  ionViewDidEnter() {
+    this.hasLoaded = null;
+    let load$ = this.loadData$();
+    this.data$ = load$;
+    load$.then(it => {
+      this.hasLoaded = (it && it.length > 0) ? "y" : "n";
+      console.log(this.hasLoaded)
+    });
+  }
+
+  private loadData$() {
+    return this.svc.initPageApiWithCallBack(this.mcontentid, () => this.refreshCallBack())
+      .then(_ => {
+        return this.svc.getApiData(this.mcontentid);
+      })
+  }
+
+  private refreshCallBack() {
+    this.hasLoaded = null;
+    let load$ = this.loadData$();
+    this.data$ = load$;
+    load$.then(it => {
+      this.hasLoaded = (it && it.length > 0) ? "y" : "n";
+      console.log(this.hasLoaded)
+    });
+  }
+
+  public onSelectItem(url: string) {
+    this.svc.visitEndpoint(this.mcontentid, url);
+  }
 }
