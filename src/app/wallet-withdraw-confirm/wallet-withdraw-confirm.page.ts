c27f22818bd6dbcd359c98ed9fc292d8096e7f6f
perawatt <pekung4433@perfenterprise.com>
2020-04-17 10:29:48 +0700

    binding wallet-withdraw-confirm

--- src/app/wallet-withdraw-confirm/wallet-withdraw-confirm.page.ts
+++ src/app/wallet-withdraw-confirm/wallet-withdraw-confirm.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { IonManaLib } from 'ion-m-lib';
+import { ParseDataProvider } from 'src/providers/parse-data';
 
 @Component({
   selector: 'app-wallet-withdraw-confirm',
@@ -7,9 +9,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WalletWithdrawConfirmPage implements OnInit {
 
-  constructor() { }
+  public hasLoaded: string;
+  public data$ = Promise.resolve<{}>({});
+  private mcontentid = "wallet-withdraw-confirm";
+  constructor(private svc: IonManaLib, private parse: ParseDataProvider) { }
 
   ngOnInit() {
   }
 
+  ionViewDidEnter() {
+    this.hasLoaded = null;
+    let load$ = this.loadData$();
+    this.data$ = load$;
+    load$.then(it => {
+      this.hasLoaded = it ? "y" : "n";
+    });
+  }
+
+  private loadData$() {
+    return this.svc.initPageApi(this.mcontentid)
+      .then(_ => {
+        return this.svc.getApiData(this.mcontentid);
+      })
+  }
+
+  public ParseToTwoDecimal(value: number) { return this.parse.ParseToTwoDecimal(value); }
 }
