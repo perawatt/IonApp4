355e995fcc97b8dd3c75eef1225227e87d9d0f03
Anawat <anawat@perfenterprise.com>
2020-04-17 12:11:32 +0700

    implement merchant home UI

--- src/app/merchant-home/merchant-home.page.ts
+++ src/app/merchant-home/merchant-home.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { IonManaLib } from 'ion-m-lib';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-merchant-home',
@@ -7,9 +9,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MerchantHomePage implements OnInit {
 
-  constructor() { }
+  public title = "ร้านของคุณ";
+  public hasLoaded: string;
+  public data$ = Promise.resolve<{}>({});
+  public slideOpts = {
+    slidesPerView: window.screen.width/100,
+    spaceBetween: 12,
+    speed: 400
+  };
+  private mcontentid = "merchant-home";
+  private merchantId: string;
+
+  constructor(private router: Router, private svc: IonManaLib) { }
 
   ngOnInit() {
   }
 
+  public onSelectReceiveMoneyQR() {
+    this.router.navigate(["/merchant-qr-receive-money"]);
+  }
+
+  public onSelectAddThirdParty() {
+    this.svc.visitEndpoint(this.mcontentid, "https://s.manal.ink/msg/underconstruction");
+  }
+
+  public onSelectProfile() {
+    this.router.navigate(["/merchant-profile"]);
+  }
+
+  public onSelectTransaction() {
+    this.router.navigate(["/merchant-transaction"]);
+  }
+
+  public onSelectWithDraw() {
+    let id = "nbizwdw-" + this.merchantId;
+    this.svc.visitEndpoint(this.mcontentid, "https://s.manal.ink/bizcenter/withdraw/" + id);
+  }
+
+  public onSelectTopup() {
+    let id = "nbizdpt-" + this.merchantId;
+    this.svc.visitEndpoint(this.mcontentid, "https://s.manal.ink/bizcenter/deposit/" + id);
+  }
+
+  public onSelectOwners() {
+    this.router.navigate(["/merchant-co-owners"]);
+  }
+
+  public onSelectSetting() {
+    this.svc.visitEndpoint(this.mcontentid, "https://s.manal.ink/msg/underconstruction");
+  }
 }
