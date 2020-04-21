71e067574d8126de8b1b499fc10fd0297b0bea22
Anawat <anawat@perfenterprise.com>
2020-04-21 09:44:53 +0700

    binding merchant create page

--- src/app/merchant-create/merchant-create.page.ts
+++ src/app/merchant-create/merchant-create.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, ValidatorFn, AbstractControl } from '@angular/forms';
+import { IonManaLib } from 'ion-m-lib';
 
 @Component({
   selector: 'app-merchant-create',
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MerchantCreatePage implements OnInit {
 
-  constructor() { }
+  public fg: FormGroup;
+  private mcontentid = "merchant-create";
+
+  constructor(private fb: FormBuilder, private svc: IonManaLib) {
+  this.fg = this.fb.group({
+    'merchantName': [null, [Validators.required, this.nullOrWhiteSpaceValidator()]],
+  });
+  }
 
   ngOnInit() {
   }
 
+  ionViewDidEnter() {
+    this.svc.initPageApi(this.mcontentid);
+  }
+
+  nullOrWhiteSpaceValidator(): ValidatorFn {
+    return (control: AbstractControl): { [key: string]: any } | null => {
+      var isNullOrWhiteSpace = control.value === null || control.value.match(/^ *$/) !== null;
+      return isNullOrWhiteSpace ? { 'merchantName': { value: control.value } } : null;
+    };
+  }
+
+  onSave() {
+    this.svc.submitFormData(this.mcontentid, this.fg.value, false);
+  }
 }
