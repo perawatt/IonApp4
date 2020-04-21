11371d10b82f96876cbf6c36e70b528a451534dd
Anawat <anawat@perfenterprise.com>
2020-04-21 16:32:52 +0700

    binding merchant select page
    Signed-off-by: Anawat <anawat@perfenterprise.com>

--- src/app/merchant-select/merchant-select.page.ts
+++ src/app/merchant-select/merchant-select.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { IonManaLib } from 'ion-m-lib';
 
 @Component({
   selector: 'app-merchant-select',
@@ -7,9 +8,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MerchantSelectPage implements OnInit {
 
-  constructor() { }
+  public hasLoaded: string;
+  public mcontentid = "merchant-select";
+  private data$ = Promise.resolve<{}>({});
+
+  constructor(private svc: IonManaLib) { }
 
   ngOnInit() {
   }
 
+  ionViewDidEnter() {
+    this.svc.addToolbarAction((action) => this.onTabToolbarItem(action));
+    this.refreshCallBack();
+  }
+
+  private loadData$() {
+    return this.svc.initPageApiWithCallBack(this.mcontentid, () => this.refreshCallBack())
+      .then(_ => {
+        return this.svc.getApiData(this.mcontentid);
+      })
+  }
+
+  refreshCallBack() {
+    this.hasLoaded = null;
+    let load$ = this.loadData$();
+    this.data$ = load$;
+    load$.then((it: any) => {
+      console.log(it)
+      this.hasLoaded = (it && it.merchantAccounts.length > 0) ? "y" : "n";
+    });
+  }
+
+  public onTabToolbarItem(action) {
+    switch (action) {
+      case "Add": this.Add(); break;
+      default: break;
+    }
+  }
+
+  public Add() {
+    this.svc.visitEndpoint(this.mcontentid, "https://s.manal.ink/merchant/nbizcre-merchant");
+  }
+
+  public onSelectItem(url: string, isActivate: boolean) {
+    if (isActivate) {
+      this.svc.visitEndpoint(this.mcontentid, url);
+    }
+  }
 }
