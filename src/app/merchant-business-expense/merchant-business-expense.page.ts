5f7e7faf79d6ad9d00374dbbdda373adcc63766a
Anawat <anawat@perfenterprise.com>
2020-04-21 10:03:11 +0700

    binding merchant business expanse page

--- src/app/merchant-business-expense/merchant-business-expense.page.ts
+++ src/app/merchant-business-expense/merchant-business-expense.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { IonManaLib } from 'ion-m-lib';
+import { ParseDataProvider } from 'src/providers/parse-data';
 
 @Component({
   selector: 'app-merchant-business-expense',
@@ -7,9 +9,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MerchantBusinessExpensePage implements OnInit {
 
-  constructor() { }
+  private hasLoaded: string;
+  private mcontentid = "merchant-business-expense";
+  public data$ = Promise.resolve<{}>({});
+
+  constructor(private svc: IonManaLib, private parse: ParseDataProvider) { }
 
   ngOnInit() {
   }
 
+  ionViewDidEnter() {
+    this.hasLoaded = null;
+    let load$ = this.loadData$();
+    this.data$ = load$;
+    load$.then(it => {
+      this.svc.initPageApi(this.mcontentid);
+      this.hasLoaded = it ? "y" : "n";
+    });
+  }
+
+  private loadData$() {
+    return this.svc.initPageApi(this.mcontentid)
+      .then(_ => {
+        return this.svc.getApiData(this.mcontentid);
+      })
+  }
+
+  public AbsoluteNumber(value: number) { return Math.abs(value); }
+
+  public ParseToTwoDecimal(value: number) { return this.parse.ParseToTwoDecimal(value); }
 }
