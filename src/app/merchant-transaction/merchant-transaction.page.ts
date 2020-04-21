ef88b66f708e823e54b5feb20f371b089d82b7d5
Anawat <anawat@perfenterprise.com>
2020-04-21 15:54:01 +0700

    binding merchant transaction page

--- src/app/merchant-transaction/merchant-transaction.page.ts
+++ src/app/merchant-transaction/merchant-transaction.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { IonManaLib } from 'ion-m-lib';
+import { ParseDataProvider } from 'src/providers/parse-data';
 
 @Component({
   selector: 'app-merchant-transaction',
@@ -7,9 +9,66 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MerchantTransactionPage implements OnInit {
 
-  constructor() { }
+  public title = "บัญชีร้าน";
+  public hasLoaded: string;
+  public data$ = Promise.resolve<{}>({});
+  private mcontentid = "merchant-transaction";
+
+  constructor(private svc: IonManaLib, private parse: ParseDataProvider) { }
 
   ngOnInit() {
   }
 
+  ionViewDidEnter() {
+    this.hasLoaded = null;
+    let load$ = this.loadData$();
+    this.data$ = load$;
+    load$.then(it => {
+      this.title = it.merchantAccount.name;
+      this.svc.initPageApi(this.mcontentid);
+      this.hasLoaded = it.transactions && it.transactions.length != 0 ? "y" : "n";
+    });
+  }
+
+  private loadData$() {
+    return this.svc.initPageApi(this.mcontentid)
+      .then(_ => {
+        return this.svc.getApiData(this.mcontentid);
+      })
+  }
+
+  doRefresh(event: { complete: () => void; }) {
+    setTimeout(() => {
+      this.loadData$();
+      event.complete();
+    }, 500);
+  }
+
+  getActionDisplay(txType: string) {
+    switch (txType.toLowerCase()) {
+      case "deposit": return "เติมเงินเข้าร้าน";
+      case "paycart": return "รับเงิน";
+      case "withdraw": return "ถอนเงิน";
+      case "autowithdraw": return "ถอนเงิน";
+      case "payback": return "เงินออก";
+      default: return "";
+    }
+  }
+
+  getActionImage(txType: string) {
+    switch (txType.toLowerCase()) {
+      case "deposit": return "assets/imgs/txrshoptopup.png";
+      case "paycart": return "assets/imgs/txrin.png";
+      case "withdraw": return "assets/imgs/txrwithdraw.png";
+      case "autowithdraw": return "assets/imgs/txrwithdraw.png";
+      case "payback": return "assets/imgs/txrout.png";
+      default: return "";
+    }
+  }
+
+  visitEndpoint(url) {
+    this.svc.visitEndpoint(this.mcontentid, url);
+  }
+
+  public ParseToTwoDecimal(value: number) { return this.parse.ParseToTwoDecimal(value); }
 }
