eb8240dbbaca4ecf7a58840ac1bb7c0e5e4b156a
Anawat <anawat@perfenterprise.com>
2020-04-22 14:08:55 +0700

    binding shopping cart info page

--- src/app/shopping-cart-info/shopping-cart-info.page.ts
+++ src/app/shopping-cart-info/shopping-cart-info.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { IonManaLib } from 'ion-m-lib';
 
 @Component({
   selector: 'app-shopping-cart-info',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppingCartInfoPage implements OnInit {
 
-  constructor() { }
+  private hasLoaded: string;
+  private mcontentid: string = "shopping-cart-info";
+  private data$ = Promise.resolve<{}>({});
+
+  constructor(private svc: IonManaLib) { }
 
   ngOnInit() {
   }
 
+  // ionViewDidEnter() {
+  //   this.hasLoaded = null;
+  //   let load$ = this.loadData$();
+  //   this.data$ = load$;
+  //   load$.then(it => {
+  //     this.svc.initPageApi(this.mcontentid);
+  //     this.hasLoaded = it ? "y" : "n";
+  //   });
+  // }
+
+  // private loadData$() {
+  //   return this.svc.initPageApi(this.mcontentid)
+  //     .then(_ => {
+  //       return this.svc.getApiData(this.mcontentid);
+  //     })
+  // }
 }
