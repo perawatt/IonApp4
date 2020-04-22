1576790c92df68b3cbcb79f12e3071a6069059b9
Anawat <anawat@perfenterprise.com>
2020-04-22 15:12:21 +0700

    binding shopping carts page

--- src/app/shopping-carts/shopping-carts.page.ts
+++ src/app/shopping-carts/shopping-carts.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { IonManaLib } from 'ion-m-lib';
 
 @Component({
   selector: 'app-shopping-carts',
@@ -7,9 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppingCartsPage implements OnInit {
 
-  constructor() { }
+  public hasLoaded: string;
+  public data$ = Promise.resolve<{}>({});
+  private mcontentid: string = "shopping-carts";
+
+  constructor(private svc: IonManaLib) { }
 
   ngOnInit() {
   }
 
+  ionViewDidEnter() {
+    this.hasLoaded = null;
+    let load$ = this.loadData$();
+    this.data$ = load$;
+    load$.then(it => {
+      this.hasLoaded = (it && it.length > 0) ? "y" : "n";
+    });
+  }
+  
+  private loadData$() {
+    return this.svc.initPageApi(this.mcontentid)
+      .then(_ => {
+        return this.svc.getApiData(this.mcontentid);
+      })
+  }
+
+  public onSelectItem(url: string) {
+    this.svc.visitEndpoint(this.mcontentid, url);
+  }
 }
