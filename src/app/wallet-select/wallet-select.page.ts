70fe05e9b17dcdeee8fb4907278987f827d03731
perawatt <pekung4433@perfenterprise.com>
2020-04-29 15:06:09 +0700

    biding wallet-select

--- src/app/wallet-select/wallet-select.page.ts
+++ src/app/wallet-select/wallet-select.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { IonManaLib } from 'ion-m-lib';
+import { ParseDataProvider } from 'src/providers/parse-data';
 
 @Component({
   selector: 'app-wallet-select',
@@ -7,9 +9,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WalletSelectPage implements OnInit {
 
-  constructor() { }
+  public hasLoaded: string;
+  public data$ = Promise.resolve<{}>({});
+  private mcontentid: string = "wallet-select";
+  constructor(private svc: IonManaLib, private parse: ParseDataProvider) { }
 
   ngOnInit() {
   }
+
+  ionViewDidEnter() {
+    this.hasLoaded = null;
+    let load$ = this.loadData$();
+    this.data$ = load$;
+    load$.then(it => {
+      this.hasLoaded = (it && it.length > 0) ? "y" : "n";
+    });
+  }
+
+  private loadData$() {
+    return this.svc.initPageApi(this.mcontentid)
+      .then(_ => {
+        return this.svc.getApiData(this.mcontentid);
+      })
+  }
+
+  public onSelectItem(id: string) {
+    this.svc.visitEndpoint(this.mcontentid, "https://s.manal.ink/wallet/defaultwallet/" + id);
+  }
+
+  public ParseToTwoDecimal(value: number) { return this.parse.ParseToTwoDecimal(value); }
 
 }
