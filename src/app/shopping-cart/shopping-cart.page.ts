7d1ab57242a4f6ee0452b64f6239909f2b0d851e
perawatt <pekung4433@perfenterprise.com>
2020-06-02 17:56:58 +0700

    style shopping-cart

--- src/app/shopping-cart/shopping-cart.page.ts
+++ src/app/shopping-cart/shopping-cart.page.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppingCartPage implements OnInit {
 
+  public title: string = "สั่งสินค้า";
   constructor() { }
 
   ngOnInit() {
