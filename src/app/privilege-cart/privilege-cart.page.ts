f093381eac52bc87eef614ab6860ed72a0ebd934
perawatt <pekung4433@perfenterprise.com>
2020-06-11 17:42:28 +0700

    implement privilege-cart

--- src/app/privilege-cart/privilege-cart.page.ts
+++ src/app/privilege-cart/privilege-cart.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { IonManaLib } from 'ion-m-lib';
 
 @Component({
   selector: 'app-privilege-cart',
@@ -6,10 +7,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./privilege-cart.page.scss'],
 })
 export class PrivilegeCartPage implements OnInit {
-
-  constructor() { }
+  
+  public mcontentid: string = "privilege-cart";
+  public hasLoaded: string;
+  private data$ = Promise.resolve<{}>({});
+  constructor(private svc: IonManaLib) { }
 
   ngOnInit() {
+  }
+
+  ionViewDidEnter() {
+    this.hasLoaded = null;
+    let load$ = this.loadData$();
+    this.data$ = load$;
+    load$.then(it => {
+      this.svc.initPageApi(this.mcontentid);
+      this.hasLoaded = it ? "y" : "n";
+      console.log(it);
+      
+    });
+  }
+
+  private loadData$() {
+    return this.svc.initPageApi(this.mcontentid)
+      .then(_ => {
+        return this.svc.getApiData(this.mcontentid);
+      })
   }
 
 }
