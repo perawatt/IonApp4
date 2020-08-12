e924c4a1008c165edfe75deebadcde2a546003ee
perawatt <pekung4433@perfenterprise.com>
2020-08-12 11:25:08 +0700

    implement searchbar autofocus

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { IonSearchbar } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -8,31 +9,49 @@ import { Component } from '@angular/core';
 export class HomePage {
   isItemAvailable = false;
   items: SearchListItem[] = [];
-  filterd = [];
+  defaultItem: SearchListItem[] = [];
+  filterdItems = [];
   constructor() {
-    this.items.push({ id: "1", category: "shop", type: "histories", logo: "", searchText: "บ้านมอ" });
-    this.items.push({ id: "4", category: "shop", type: "histories", logo: "", searchText: "Coffee De La" });
-    this.items.push({ id: "5", category: "shop", type: "histories", logo: "", searchText: "แล้วแต่ กะเพราแท้" });
-    this.items.push({ id: "7", category: "shop", type: "histories", logo: "", searchText: "ย่างเพราเห่าดง" });
-    this.items.push({ id: "9", category: "shop", type: "histories", logo: "", searchText: "Grus Cafe" });
-    this.items.push({ id: "2", category: "shop", type: "suggestion", logo: "", searchText: "มานะ" });
-    this.items.push({ id: "3", category: "shop", type: "suggestion", logo: "", searchText: "ก๋วยเตี๋ยวสิบสาม" });
-    this.items.push({ id: "6", category: "shop", type: "suggestion", logo: "", searchText: "ย่างเพรา เห่าดง" });
-    this.items.push({ id: "8", category: "third", type: "suggestion", logo: "", searchText: "Promome" });
+    this.items.push({ id: "1", category: "shop", type: "histories", logo: "assets/imgs/search-clock.png", description: "โปรโมชัน", name: "โปรโมชัน" });
+    this.items.push({ id: "2", category: "shop", type: "histories", logo: "assets/imgs/search-clock.png", description: "ร้านคอฟฟี่เดอลา", name: "Coffee De La" });
+    this.items.push({ id: "3", category: "shop", type: "histories", logo: "assets/imgs/search-clock.png", description: "โปรโมชันอาหาร", name: "โปรโมชันอาหาร" });
+    this.items.push({ id: "4", category: "shop", type: "histories", logo: "assets/imgs/search-clock.png", description: "ร้านย่างเพราเห่าดง", name: "ย่างเพราเห่าดง" });
+    this.items.push({ id: "5", category: "shop", type: "histories", logo: "assets/imgs/search-clock.png", description: "ร้านกรัส คาเฟ่", name: "Grus Cafe" });
+    this.items.push({ id: "9", category: "third", type: "histories", logo: "assets/imgs/search-clock.png", description: "โปรโมมี", name: "Promome โปรโมมี" });
+    this.items.push({ id: "6", category: "shop", type: "suggestion", logo: "assets/imgs/search-magnifier.png", description: "ร้านอาหารหลังมอ", name: "ร้านอาหารหลังมอ" });
+    this.items.push({ id: "6", category: "shop", type: "suggestion", logo: "assets/imgs/search-magnifier.png", description: "ดูหนัง", name: "ดูหนัง" });
+    this.items.push({ id: "7", category: "shop", type: "suggestion", logo: "assets/imgs/search-magnifier.png", description: "ก๋วยเตี๋ยวสิบสาม", name: "ก๋วยเตี๋ยวสิบสาม" });
+    this.items.push({ id: "8", category: "shop", type: "suggestion", logo: "assets/imgs/search-magnifier.png", description: "ร้านย่างเพรา เห่าดง", name: "ย่างเพรา เห่าดง" });
+    this.items.push({ id: "9", category: "third", type: "suggestion", logo: "assets/imgs/search-magnifier.png", description: "โปรโมมี", name: "Promome " });
+
+    this.defaultItem.push({ id: "1", category: "shop", type: "histories", logo: "assets/imgs/search-clock.png", description: "", name: "แจกคูปอง" });
+    this.defaultItem.push({ id: "2", category: "membership", type: "membership", logo: "assets/imgs/topup.png", description: "เติมเงิน", name: "เติมเงิน" });
+    this.defaultItem.push({ id: "3", category: "third", type: "membership", logo: "assets/imgs/shop-default.png", description: "โปรโมมี", name: "Promome " });
+    this.defaultItem.push({ id: "4", category: "third", type: "histories", logo: "assets/imgs/search-clock.png", description: "ดูหนัง", name: "ดูหนัง " });
+    this.defaultItem.push({ id: "5", category: "third", type: "membership", logo: "assets/imgs/changewallet.png", description: "กระเป๋าเงิน", name: "กระเป๋าเงิน " });
+  }
+
+  @ViewChild('searchbar', { static: false }) searchbar: IonSearchbar;
+
+  ionViewDidEnter() {
+    this.searchbar.setFocus();
   }
 
   getItems(ev: any) {
     // set val to the value of the searchbar
     const val = ev.target.value;
+    console.log("onChange"+ev);
+    this.isItemAvailable = true;
 
     // if the value is an empty string don't filter the items
     if (val && val.trim() !== '') {
-      this.isItemAvailable = true;
-      this.filterd = this.items.filter((item) => {
-        return (item.searchText.toLowerCase().indexOf(val.toLowerCase()) > -1);
+      this.filterdItems = this.items.filter((item) => {
+        return (item.name.toLowerCase().indexOf(val.toLowerCase()) > -1);
       })
     } else {
-      this.isItemAvailable = false;
+      this.filterdItems = this.defaultItem;
+
+      // this.isItemAvailable = false;
     }
   }
 }
@@ -42,7 +61,8 @@ export class SearchListItem {
   id: string;
   logo: string;
   type: string;
-  searchText: string;
+  name: string;
+  description: string;
   category: string;
 }
 
