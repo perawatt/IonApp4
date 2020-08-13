45d0a0566931816f378ba2e0ab15dfc7bff2ab22
perawatt <pekung4433@perfenterprise.com>
2020-08-13 11:25:32 +0700

    add more collection

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -30,17 +30,19 @@ export class HomePage {
     this.items.push({ id: "4", tags: ["shop", "อาหาร"], type: "histories", logo: "assets/imgs/search-clock.png", description: "ร้านย่างเพราเห่าดง", name: "ย่างเพราเห่าดง" });
     this.items.push({ id: "5", tags: ["shop"], type: "histories", logo: "assets/imgs/search-clock.png", description: "ร้านกรัส คาเฟ่", name: "Grus Cafe" });
     this.items.push({ id: "6", tags: ["third"], type: "histories", logo: "assets/imgs/search-clock.png", description: "สมาชิก Promome(โปรโมมี)", name: "Promome โปรโมมี" });
+    this.items.push({ id: "12", tags: ["third"], type: "histories", logo: "assets/imgs/search-clock.png", description: "", name: "แจกคูปอง" });
+    this.items.push({ id: "13", tags: ["third"], type: "histories", logo: "assets/imgs/search-clock.png", description: "ดูหนัง", name: "ดูหนัง " });
     this.items.push({ id: "7", tags: ["shop", "Coupon"], type: "suggestion", logo: "assets/imgs/search-magnifier.png", description: "คูปองร้าน iStudio", name: "ร้าน iStudio" });
-    this.items.push({ id: "8", tags: ["shop"], type: "suggestion", logo: "assets/imgs/search-magnifier.png", description: "ดูหนัง", name: "ดูหนัง" });
     this.items.push({ id: "9", tags: ["shop", "อาหาร"], type: "suggestion", logo: "assets/imgs/search-magnifier.png", description: "ก๋วยเตี๋ยวสิบสาม", name: "ก๋วยเตี๋ยวสิบสาม" });
     this.items.push({ id: "10", tags: ["shop", "อาหาร"], type: "suggestion", logo: "assets/imgs/search-magnifier.png", description: "ร้านย่างเพรา เห่าดง", name: "ย่างเพรา เห่าดง" });
-    this.items.push({ id: "11", tags: ["third"], type: "suggestion", logo: "assets/imgs/search-magnifier.png", description: "สมาชิก Promome(โปรโมมี)", name: "Promome " });
+    this.items.push({ id: "11", tags: ["third"], type: "suggestion", logo: "assets/imgs/search-magnifier.png", description: "สมาชิก Promome(โปรโมมี)", name: "Promome" });
+
 
     this.defaultItem.push({ id: "1", tags: ["third"], type: "histories", logo: "assets/imgs/search-clock.png", description: "", name: "แจกคูปอง" });
     this.defaultItem.push({ id: "2", tags: ["mana"], type: "membership", logo: "assets/imgs/topup.png", description: "เติมเงิน", name: "เติมเงิน" });
     this.defaultItem.push({ id: "3", tags: ["membership", "promome"], type: "membership", logo: "assets/imgs/shop-default.png", description: "โปรโมมี", name: "Promome" });
     this.defaultItem.push({ id: "4", tags: ["third"], type: "histories", logo: "assets/imgs/search-clock.png", description: "ดูหนัง", name: "ดูหนัง " });
-    this.defaultItem.push({ id: "5", tags: ["third"], type: "membership", logo: "assets/imgs/changewallet.png", description: "กระเป๋าเงิน", name: "กระเป๋าเงิน " });
+    this.defaultItem.push({ id: "5", tags: ["third"], type: "membership", logo: "assets/imgs/changewallet.png", description: "กระเป๋าเงิน", name: "กระเป๋าเงิน" });
     this.defaultItem.push({ id: "6", tags: ["iStudio","Coupon"], type: "coupon", logo: "assets/imgs/privilege-info.png", description: "คูปองร้าน iStudio", name: "iStudio" });
 
     this.collectionItem.push({ id: "1", tags: ["Promome","Membership"], type: "membership", logo: "assets/imgs/shop-default.png", description: "สมาชิก Promome(โปรโมมี)", name: "Promome" });
@@ -57,6 +59,8 @@ export class HomePage {
   }
 
   getItems(ev: any) {
+    console.log(ev);
+    
     const val = ev.target.value;
     this.isSearching = true;
     if (val && val.trim() !== '') {
