7e2e7dbb4a839f2be8d1392f2d3a12648b90a6e7
tachapong <tachapong999@gmail.com>
2020-08-21 13:29:26 +0700

    binding merchantEditname && ui merchantEditCode

--- src/app/merchant-edit-code/merchant-edit-code.page.ts
+++ src/app/merchant-edit-code/merchant-edit-code.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { IonManaLib } from 'ion-m-lib';
 
 @Component({
   selector: 'app-merchant-edit-code',
@@ -6,10 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./merchant-edit-code.page.scss'],
 })
 export class MerchantEditCodePage implements OnInit {
+  public hasLoaded: string;
+  public data$ = Promise.resolve<{}>({});
+  private mcontentid = "merchant-edit-code";
 
-  constructor() { }
+
+  constructor(private svc: IonManaLib) { }
 
   ngOnInit() {
   }
 
+  ionViewDidEnter() {
+    this.hasLoaded = null;
+    let load$ = this.loadData$();
+    this.data$ = load$;
+    load$.then(it => {
+    });
+  }
+
+  private loadData$() {
+    return this.svc.initPageApi(this.mcontentid)
+      .then(_ => {
+        return this.svc.callApiGet(this.mcontentid,"https://mana-facing-dev.azurewebsites.net/BizAccount/637334894267091896/code")
+        // return this.svc.getApiData(this.mcontentid);
+      })
+  }
 }
--- src/app/merchant-edit-name/merchant-edit-name.module.ts
+++ src/app/merchant-edit-name/merchant-edit-name.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
 import { MerchantEditNamePageRoutingModule } from './merchant-edit-name-routing.module';
 
 import { MerchantEditNamePage } from './merchant-edit-name.page';
+import { DirectivesModule } from 'src/directives/directives.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
+    ReactiveFormsModule,
+    DirectivesModule,
     MerchantEditNamePageRoutingModule
   ],
   declarations: [MerchantEditNamePage]
