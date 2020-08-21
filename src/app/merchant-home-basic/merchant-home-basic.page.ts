3405a3494114a34ad0b79a6597b0b4631d98a35b
powerofwind <powerofwind10@gmail.com>
2020-08-21 15:45:46 +0700

    add refresCallBack

--- src/app/merchant-home-basic/merchant-home-basic.page.ts
+++ src/app/merchant-home-basic/merchant-home-basic.page.ts
@@ -8,33 +8,35 @@ import { IonManaLib } from 'ion-m-lib';
 })
 export class MerchantHomeBasicPage implements OnInit {
 
-  // TODO : Binding title
   public title: string = "Shop";
   public hasLoaded: string;
   private mcontentid = "merchant-home-basic";
   public data$ = Promise.resolve<{}>({});
-
   constructor(private svc: IonManaLib) { }
 
   ngOnInit() {
   }
 
   ionViewDidEnter() {
-    this.hasLoaded = null;
-    let load$ = this.loadData$();
-    this.data$ = load$;
-    load$.then(it => {
-      this.title = it.name != null ? it.name : this.title;
-      this.hasLoaded = it ? "y" : "n";
-    });
+    this.refreshCallBack();
   }
 
   private loadData$() {
-    return this.svc.initPageApi(this.mcontentid)
+    return this.svc.initPageApiWithCallBack(this.mcontentid, () => this.refreshCallBack())
       .then(_ => {
         // return this.svc.callApiGet(this.mcontentid,"http://mana-facing-dev.azurewebsites.net/BizAccount/637334893975177162/basic")
         return this.svc.getApiData(this.mcontentid);
       })
+  }
+
+  refreshCallBack() {
+    this.hasLoaded = null;
+    let load$ = this.loadData$();
+    this.data$ = load$;
+    load$.then((it: any) => {
+      this.title = it.name != null ? it.name : this.title;
+      this.hasLoaded = it ? "y" : "n";
+    });
   }
 
 }
