3a2a09d0d0b43345be245ef4b2e89de558db5673
perawatt <pekung4433@perfenterprise.com>
2020-10-02 11:20:20 +0700

    binding wallet-transaction-adhoc and wallet-transaction-ppay-payment

--- src/app/wallet-transaction-adhoc/wallet-transaction-adhoc.page.ts
+++ src/app/wallet-transaction-adhoc/wallet-transaction-adhoc.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { IonManaLib } from 'ion-m-lib';
+import { ParseDataProvider } from 'src/providers/parse-data';
 
 @Component({
   selector: 'app-wallet-transaction-adhoc',
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WalletTransactionAdhocPage implements OnInit {
 
-  constructor() { }
+  public hasLoaded: string;
+  public data$ = Promise.resolve<{}>({});
+  private mcontentid: string = "wallet-transaction-adhoc";
+  constructor(private svc: IonManaLib, private parse: ParseDataProvider) { }
 
   ngOnInit() {
   }
 
+  ionViewDidEnter() {
+    this.hasLoaded = null;
+    let load$ = this.loadData$();
+    this.data$ = load$;
+    load$.then(it => {
+      this.svc.initPageApi(this.mcontentid);
+      this.hasLoaded = (it) ? "y" : "n";
+    });
+  }
+
+  private loadData$() {
+    return this.svc.initPageApi(this.mcontentid)
+      .then(_ => {
+        return this.svc.getApiData(this.mcontentid);
+      })
+  }
+
+  public ParseToTwoDecimal(value: number) { return this.parse.ParseToTwoDecimal(value); }
 }
--- src/app/wallet-transaction-ppay-payment/wallet-transaction-ppay-payment.page.ts
+++ src/app/wallet-transaction-ppay-payment/wallet-transaction-ppay-payment.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { IonManaLib } from 'ion-m-lib';
+import { ParseDataProvider } from 'src/providers/parse-data';
 
 @Component({
   selector: 'app-wallet-transaction-ppay-payment',
@@ -8,11 +10,29 @@ import { Component, OnInit } from '@angular/core';
 export class WalletTransactionPpayPaymentPage implements OnInit {
 
   public hasLoaded: string;
-  public data$;
+  public data$ = Promise.resolve<{}>({});
   private mcontentid: string = "wallet-transaction-ppay-payment";
-  constructor() { }
+  constructor(private svc: IonManaLib, private parse: ParseDataProvider) { }
 
   ngOnInit() {
   }
 
+  ionViewDidEnter() {
+    this.hasLoaded = null;
+    let load$ = this.loadData$();
+    this.data$ = load$;
+    load$.then(it => {
+      this.svc.initPageApi(this.mcontentid);
+      this.hasLoaded = (it) ? "y" : "n";
+    });
+  }
+
+  private loadData$() {
+    return this.svc.initPageApi(this.mcontentid)
+      .then(_ => {
+        return this.svc.getApiData(this.mcontentid);
+      })
+  }
+
+  public ParseToTwoDecimal(value: number) { return this.parse.ParseToTwoDecimal(value); }
 }
