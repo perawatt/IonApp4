e5e70e1b0ed5abc7d81fcccc31d177565029921c
Anawat <anawat@perfenterprise.com>
2020-10-15 11:53:25 +0700

    add KBANK HowTo and manage image
    Signed-off-by: Anawat <anawat@perfenterprise.com>

--- src/app/wallet-topup-howto-bank-detail/wallet-topup-howto-bank-detail.page.ts
+++ src/app/wallet-topup-howto-bank-detail/wallet-topup-howto-bank-detail.page.ts
@@ -28,10 +28,12 @@ export class WalletTopupHowtoBankDetailPage implements OnInit {
           ], type: this.sourceType.step
         },
         { source: "ขั้นตอนที่ 2", type: this.sourceType.header },
-        { source: "assets/imgs/HowTo/KTB/2.3.png", type: this.sourceType.image },
+        { source: "assets/imgs/HowTo/MANA/2.3.png", type: this.sourceType.image },
         {
           source: [
             { source: "เข้าที่ App mana เพื่อส่งคำขอเติมเงิน", type: this.sourceType.text },
+            { source: "กรอกข้อมูลให้ครบถ้วน", type: this.sourceType.disc },
+            { source: "กด “ตกลง”", type: this.sourceType.disc },
           ], type: this.sourceType.step
         },
         { source: "ขั้นตอนที่ 3", type: this.sourceType.header },
@@ -49,7 +51,7 @@ export class WalletTopupHowtoBankDetailPage implements OnInit {
           ], type: this.sourceType.step
         },
         { source: "ขั้นตอนที่ 4", type: this.sourceType.header },
-        { source: "assets/imgs/HowTo/KTB/3.7.png", type: this.sourceType.image },
+        { source: "assets/imgs/HowTo/MANA/3.7.png", type: this.sourceType.image },
         {
           source: [
             { source: "ทำการเติมเงินเข้าสู่บัญชี mana สำเร็จ", type: this.sourceType.text },
@@ -58,13 +60,210 @@ export class WalletTopupHowtoBankDetailPage implements OnInit {
         },
       ]
     },
-    { name: "กสิกรไทย", bankCode: "KBANK" },
-    { name: "ไทยพาณิชย์", bankCode: "SCB" },
-    { name: "ทหารไทย", bankCode: "TMB" },
-    { name: "กรุงศรีอยุธยา", bankCode: "BAY" },
-    { name: "ธนชาต", bankCode: "TBANK" },
-    { name: "ออนสิน", bankCode: "GSB" },
-    { name: "กรุงเทพ", bankCode: "BBL" }
+    {
+      name: "กสิกรไทย", bankCode: "KBANK", detail: [
+        { source: "ขั้นตอนที่ 1", type: this.sourceType.header },
+        { source: "assets/imgs/HowTo/KBANK/1.5.jpg", type: this.sourceType.image },
+        {
+          source: [
+            { source: "ทำการเปิด “เรียกเก็บเงิน” หรือ “Request to pay”", type: this.sourceType.text },
+            { source: "เลือก “ตั้งค่า”", type: this.sourceType.disc },
+            { source: "เลือก “บัญชี/บัตร”", type: this.sourceType.disc },
+            { source: "ทำการเปิด", type: this.sourceType.disc },
+          ], type: this.sourceType.step
+        },
+        { source: "ขั้นตอนที่ 2", type: this.sourceType.header },
+        { source: "assets/imgs/HowTo/MANA/2.3.png", type: this.sourceType.image },
+        {
+          source: [
+            { source: "เข้าที่ App mana เพื่อส่งคำขอเติมเงิน", type: this.sourceType.text },
+            { source: "กรอกข้อมูลให้ครบถ้วน", type: this.sourceType.disc },
+            { source: "กด “ตกลง”", type: this.sourceType.disc },
+          ], type: this.sourceType.step
+        },
+        { source: "ขั้นตอนที่ 3", type: this.sourceType.header },
+        { source: "assets/imgs/HowTo/KBANK/3.3.jpg", type: this.sourceType.image },
+        {
+          source: [
+            { source: "เข้าสู่แอปพลิเคชัน ธนาคารกสิกรไทย", type: this.sourceType.text },
+            { source: "มีข้อความแจ้งเตือนที่ App K PLUS", type: this.sourceType.disc },
+            { source: "กดเข้าข้อความแจ้งเตือน", type: this.sourceType.disc },
+            { source: "เลือก “โอนเงิน”", type: this.sourceType.disc },
+            { source: "เลือก “ยืนยัน”", type: this.sourceType.disc },
+          ], type: this.sourceType.step
+        },
+        { source: "ขั้นตอนที่ 4", type: this.sourceType.header },
+        { source: "assets/imgs/HowTo/MANA/3.7.png", type: this.sourceType.image },
+        {
+          source: [
+            { source: "ทำการเติมเงินเข้าสู่บัญชี mana สำเร็จ", type: this.sourceType.text },
+            { source: "ได้รับการแจ้งเตือน “เติมเงินสำเร็จ”", type: this.sourceType.disc },
+          ], type: this.sourceType.step
+        },
+      ]
+    },
+    {
+      name: "ไทยพาณิชย์", bankCode: "SCB", detail: [
+        {
+          source: [
+            //TODO: 1st step
+            { source: "ขั้นตอนที่ 2", type: this.sourceType.header },
+            { source: "assets/imgs/HowTo/MANA/2.3.png", type: this.sourceType.image },
+            {
+              source: [
+                { source: "เข้าที่ App mana เพื่อส่งคำขอเติมเงิน", type: this.sourceType.text },
+                { source: "กรอกข้อมูลให้ครบถ้วน", type: this.sourceType.disc },
+                { source: "กด “ตกลง”", type: this.sourceType.disc },
+              ], type: this.sourceType.step
+            },
+            //TODO: 3nd step
+            { source: "ขั้นตอนที่ 4", type: this.sourceType.header },
+            { source: "assets/imgs/HowTo/MANA/3.7.png", type: this.sourceType.image },
+            {
+              source: [
+                { source: "ทำการเติมเงินเข้าสู่บัญชี mana สำเร็จ", type: this.sourceType.text },
+                { source: "ได้รับการแจ้งเตือน “เติมเงินสำเร็จ”", type: this.sourceType.disc },
+              ], type: this.sourceType.step
+            },
+          ], type: this.sourceType.step
+        }
+      ]
+    },
+    {
+      name: "ทหารไทย", bankCode: "TMB", detail: [
+        {
+          source: [
+            //TODO: 1st step
+            { source: "ขั้นตอนที่ 2", type: this.sourceType.header },
+            { source: "assets/imgs/HowTo/MANA/2.3.png", type: this.sourceType.image },
+            {
+              source: [
+                { source: "เข้าที่ App mana เพื่อส่งคำขอเติมเงิน", type: this.sourceType.text },
+                { source: "กรอกข้อมูลให้ครบถ้วน", type: this.sourceType.disc },
+                { source: "กด “ตกลง”", type: this.sourceType.disc },
+              ], type: this.sourceType.step
+            },
+            //TODO: 3nd step
+            { source: "ขั้นตอนที่ 4", type: this.sourceType.header },
+            { source: "assets/imgs/HowTo/MANA/3.7.png", type: this.sourceType.image },
+            {
+              source: [
+                { source: "ทำการเติมเงินเข้าสู่บัญชี mana สำเร็จ", type: this.sourceType.text },
+                { source: "ได้รับการแจ้งเตือน “เติมเงินสำเร็จ”", type: this.sourceType.disc },
+              ], type: this.sourceType.step
+            },
+          ], type: this.sourceType.step
+        }
+      ]
+    },
+    {
+      name: "กรุงศรีอยุธยา", bankCode: "BAY", detail: [
+        {
+          source: [
+            //TODO: 1st step
+            { source: "ขั้นตอนที่ 2", type: this.sourceType.header },
+            { source: "assets/imgs/HowTo/MANA/2.3.png", type: this.sourceType.image },
+            {
+              source: [
+                { source: "เข้าที่ App mana เพื่อส่งคำขอเติมเงิน", type: this.sourceType.text },
+                { source: "กรอกข้อมูลให้ครบถ้วน", type: this.sourceType.disc },
+                { source: "กด “ตกลง”", type: this.sourceType.disc },
+              ], type: this.sourceType.step
+            },
+            //TODO: 3nd step
+            { source: "ขั้นตอนที่ 4", type: this.sourceType.header },
+            { source: "assets/imgs/HowTo/MANA/3.7.png", type: this.sourceType.image },
+            {
+              source: [
+                { source: "ทำการเติมเงินเข้าสู่บัญชี mana สำเร็จ", type: this.sourceType.text },
+                { source: "ได้รับการแจ้งเตือน “เติมเงินสำเร็จ”", type: this.sourceType.disc },
+              ], type: this.sourceType.step
+            },
+          ], type: this.sourceType.step
+        }
+      ]
+    },
+    {
+      name: "ธนชาต", bankCode: "TBANK", detail: [
+        {
+          source: [
+            //TODO: 1st step
+            { source: "ขั้นตอนที่ 2", type: this.sourceType.header },
+            { source: "assets/imgs/HowTo/MANA/2.3.png", type: this.sourceType.image },
+            {
+              source: [
+                { source: "เข้าที่ App mana เพื่อส่งคำขอเติมเงิน", type: this.sourceType.text },
+                { source: "กรอกข้อมูลให้ครบถ้วน", type: this.sourceType.disc },
+                { source: "กด “ตกลง”", type: this.sourceType.disc },
+              ], type: this.sourceType.step
+            },
+            //TODO: 3nd step
+            { source: "ขั้นตอนที่ 4", type: this.sourceType.header },
+            { source: "assets/imgs/HowTo/MANA/3.7.png", type: this.sourceType.image },
+            {
+              source: [
+                { source: "ทำการเติมเงินเข้าสู่บัญชี mana สำเร็จ", type: this.sourceType.text },
+                { source: "ได้รับการแจ้งเตือน “เติมเงินสำเร็จ”", type: this.sourceType.disc },
+              ], type: this.sourceType.step
+            },
+          ], type: this.sourceType.step
+        }
+      ]
+    },
+    {
+      name: "ออนสิน", bankCode: "GSB", detail: [
+        {
+          source: [
+            //TODO: 1st step
+            { source: "ขั้นตอนที่ 2", type: this.sourceType.header },
+            { source: "assets/imgs/HowTo/MANA/2.3.png", type: this.sourceType.image },
+            {
+              source: [
+                { source: "เข้าที่ App mana เพื่อส่งคำขอเติมเงิน", type: this.sourceType.text },
+                { source: "กรอกข้อมูลให้ครบถ้วน", type: this.sourceType.disc },
+                { source: "กด “ตกลง”", type: this.sourceType.disc },
+              ], type: this.sourceType.step
+            },
+            //TODO: 3nd step
+            { source: "ขั้นตอนที่ 4", type: this.sourceType.header },
+            { source: "assets/imgs/HowTo/MANA/3.7.png", type: this.sourceType.image },
+            {
+              source: [
+                { source: "ทำการเติมเงินเข้าสู่บัญชี mana สำเร็จ", type: this.sourceType.text },
+                { source: "ได้รับการแจ้งเตือน “เติมเงินสำเร็จ”", type: this.sourceType.disc },
+              ], type: this.sourceType.step
+            },
+          ], type: this.sourceType.step
+        }
+      ]
+    },
+    {
+      name: "กรุงเทพ", bankCode: "BBL", detail: [
+        {
+          source: [
+            //TODO: 1st step
+            { source: "ขั้นตอนที่ 2", type: this.sourceType.header },
+            { source: "assets/imgs/HowTo/MANA/2.3.png", type: this.sourceType.image },
+            {
+              source: [
+                { source: "เข้าที่ App mana เพื่อส่งคำขอเติมเงิน", type: this.sourceType.text },
+                { source: "กรอกข้อมูลให้ครบถ้วน", type: this.sourceType.disc },
+                { source: "กด “ตกลง”", type: this.sourceType.disc },
+              ], type: this.sourceType.step
+            },
+            //TODO: 3nd step
+            { source: "ขั้นตอนที่ 4", type: this.sourceType.header },
+            { source: "assets/imgs/HowTo/MANA/3.7.png", type: this.sourceType.image },
+            {
+              source: [
+                { source: "ทำการเติมเงินเข้าสู่บัญชี mana สำเร็จ", type: this.sourceType.text },
+                { source: "ได้รับการแจ้งเตือน “เติมเงินสำเร็จ”", type: this.sourceType.disc },
+              ], type: this.sourceType.step
+            },
+          ], type: this.sourceType.step
+        }
+      ]
+    }
   ];
   constructor(private activatedRoute: ActivatedRoute, private svc: IonManaLib) { }
 
