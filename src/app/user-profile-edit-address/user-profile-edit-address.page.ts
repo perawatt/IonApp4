959229acc9e3d41985b450a296f47e0582888ef5
Anawat <anawat@perfenterprise.com>
2020-12-11 15:29:43 +0700

    supported fill address from xamarin
    Signed-off-by: Anawat <anawat@perfenterprise.com>

--- src/app/user-profile-edit-address/user-profile-edit-address.page.ts
+++ src/app/user-profile-edit-address/user-profile-edit-address.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, NgZone, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { IonManaLib } from 'ion-m-lib';
 
@@ -14,7 +14,8 @@ export class UserProfileEditAddressPage implements OnInit {
   public formData$: Promise<{}> = new Promise<{}>(_ => { });
   private mcontentid = "user-profile-edit-address";
 
-  constructor(private fb: FormBuilder, private svc: IonManaLib) {
+  constructor(private fb: FormBuilder, private zone: NgZone, private svc: IonManaLib) {
+    (<any>window).fillAddress = (params: any) => { this.fillAddress(params) };
     this.fg = this.fb.group({
       'address': this.fb.group({
         '_id': null,
@@ -58,6 +59,16 @@ export class UserProfileEditAddressPage implements OnInit {
       .then(_ => {
         return this.svc.getApiData(this.mcontentid);
       })
+  }
+
+
+  fillAddress(params: any) {
+    this.zone.run(() => {
+      this.fg.get('address').get('district').setValue(params.SubDistrict);
+      this.fg.get('address').get('city').setValue(params.District);
+      this.fg.get('address').get('province').setValue(params.Province);
+      this.fg.get('address').get('postalCode').setValue(params.PostalCode);
+    })
   }
 
   onSave() {
