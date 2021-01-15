9213e0f045a898be477539b4ecfb5098675463e2
Earn <earndarkness@perfenterprise.com>
2021-01-15 15:27:50 +0700

    Implement topup flows (ppay)

--- src/app/wallet-topup-ppay-confirm/wallet-topup-ppay-confirm.page.ts
+++ src/app/wallet-topup-ppay-confirm/wallet-topup-ppay-confirm.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { IonManaLib } from 'ion-m-lib';
 
 @Component({
   selector: 'app-wallet-topup-ppay-confirm',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WalletTopupPpayConfirmPage implements OnInit {
 
-  constructor() { }
+  public hasLoaded: string;
+  public data$ = Promise.resolve<{}>({});
+  private mcontentid = "wallet-topup-ppay-confirm";
+
+  constructor(private svc: IonManaLib) { }
 
   ngOnInit() {
+  }
+
+  ionViewDidEnter() {
+    this.hasLoaded = null;
+    let load$ = this.loadData$();
+    this.data$ = load$;
+    load$.then(it => {
+      this.hasLoaded = it ? "y" : "n";
+    });
+  }
+
+  private loadData$() {
+    return this.svc.initPageApi(this.mcontentid)
+      .then(_ => {
+        return this.svc.getApiData(this.mcontentid);
+      })
   }
 
 }
